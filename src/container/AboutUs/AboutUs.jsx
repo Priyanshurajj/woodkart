33acb7745bd7af8bc1b49768b1fc977a334845b6
Priyanshu_raj <rpriyanshu758@gmail.com>
2022-07-03 01:58:11 +0530

    I made this cahnge

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,17 +5,12 @@ import './AboutUs.css';
 
 const AboutUs = () => (
   <div className="app__aboutus app__bg flex__center section__padding" id="about">
-    
-    {/* <div className="app__aboutus-overlay flex__center">
-      <img src={images.G} alt="g-letter"/>
-    </div> */}
-
 
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about-sponn" className="spoon__img"/>
-          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sint nihil!</p>
+          <p className="p__opensans">What started small,with a single small discount store and a simple idea of sell more for less has grown over the last 50 years as one of the biggest E-commerce plateform of world.Each week nearly 12 million customers and members visit approximately 11,500 stores under 56 banners in 27 countries.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
 
@@ -27,7 +22,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about-sponn" className="spoon__img"/>
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eaque sint nihil!</p>
+        <p className="p__opensans">WoodKart employs over 2.2 million associates worldwide. WoodKart continues to be a leader in sustainability, corporate philanthropy and employment opportunity.It's all part of our unwavering commitment to creating opportunities and bringing value to customers and communities around the world.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
     </div>
