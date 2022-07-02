e054f0b7e59c5cf0e34841bfeef1d1b48efc1058
Priyanshu_raj <rpriyanshu758@gmail.com>
2022-07-03 02:55:10 +0530

    commit2

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new Trends"/>
       <h1 className="app__header-h1">Stay Home and Shop Online</h1>
-      <p className="p__opensans" style={{margin: '2rem 0'}}>Every Purchase will be Made With Pleasure</p>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>Every Purchase will be Made With Pleasure..</p>
       <button type="button" className="custom__button"><a href="#menu">Explore Menu</a></button>
     </div>
 
